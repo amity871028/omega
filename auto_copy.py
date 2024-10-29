64a2305d48cc8a7e3888df7c86676c85ebe43044
amity.lin <amity.lin@quantatw.com>
2024-10-29 13:33:58 +0800

    fix: adjust cursor clicking pos & method of geting request

--- auto_copy.py
+++ auto_copy.py
@@ -4,9 +4,12 @@ import time
 from PIL import ImageGrab
 
 def get_string():
+    pyautogui.moveTo(476, 788, duration=1) # prevent unread text display
+    pyautogui.click() # focus on the window
+    time.sleep(0.5)
+
     # first time to find text
     pyautogui.moveTo(230, 732, duration=1)
-    pyautogui.click() # focus on the window
 
     time.sleep(1)
     pyautogui.click(clicks=3) # select all
@@ -40,11 +43,24 @@ def get_string():
         print("error:", e)
 
 def get_request(text):
-    if '單號' in text and '+2' in text:
-        number = text[text.index('單號') + len('單號'):text.index('單號') + len('單號') + 5]
-        operator = text[text.index('+2') - 1]
+    if ('０' in text or '0' in text) and ('高' in text or '低' in text):
+        if '0' in text:
+            number = text[text.index('0') + len('0') - 1:text.index('0') + len('0') + 4]
+        else:
+            number = text[text.index('０') + len('０') - 1:text.index('０') + len('０') + 4]
+            number = number.translate(str.maketrans('０１２３４５６７８９', '0123456789'))
+        if '高' in text:
+            operator = '高'
+        else:
+            operator = '低'
         return number, operator
     return None, None
 
+# print(pyautogui.position())
 # get_string()
 # print(get_request(get_string()))
+# from datetime import datetime
+# prev_time = datetime.now()
+# time.sleep(10)
+# crt_time = datetime.now()
+# print((crt_time - prev_time).total_seconds())
